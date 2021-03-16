5d064212c6901a13c281730784e943a2abcc863c
blue-1-star <firstconnor@hotmail.com>
2021-03-16 21:08:17 +0200

    4 new files

---
+++ Kantor/Src/knS_p_064b.js
@@ -0,0 +1,6 @@
+
+	let login = "ohjyoij";
+	let message = 	(login == "employee") ? "Hi" :     
+				(login == "Director") ? "Hello!"  :
+				(login == "")  ? "No login" : "__";  
+	alert( message);	
